ea3623fa7e1de57a66d8857a7844f8dbc064a306
Maxwell Krohn <themax@gmail.com>
2014-12-30 15:41:17 -0500

    trying a little bit here

---
+++ rpc2/example/client.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+//	"github.com/maxtaco/go-framed-msgpack-rpc/rpc2"
+)
+
+type Client struct {
+}
+
+func (s *Client) Run() error {
+	return nil
+}
---
+++ rpc2/example/server.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+//	"github.com/maxtaco/go-framed-msgpack-rpc/rpc2"
+)
+
+type Server struct {
+}
+
+func (s *Server) Run() error {
+	return nil
+}
