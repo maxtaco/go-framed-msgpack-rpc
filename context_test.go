7fbe27ccb6ed4bf5c5f02e5ced46ce46df4a3819
John Zila <john@jzila.com>
2016-01-05 15:25:50 -0800

    Added codec and context changes to allow RPC debug tags
    Signed-off-by: John Zila <john@jzila.com>

---
+++ context_test.go
@@ -0,0 +1,27 @@
+package rpc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
+)
+
+func TestRpcTags(t *testing.T) {
+	logTags := make(CtxRpcTags)
+
+	logTags["hello"] = "world"
+	ctx := AddRpcTagsToContext(context.Background(), logTags)
+
+	logTags2 := make(CtxRpcTags)
+	logTags2["hello"] = "world2"
+	ctx = AddRpcTagsToContext(ctx, logTags2)
+
+	logTags, _ = RpcTagsFromContext(ctx)
+	require.Equal(t, "world2", logTags["hello"])
+
+	outTags, ok := RpcTagsFromContext(ctx)
+
+	require.Equal(t, true, ok)
+	require.Equal(t, logTags, outTags)
+}
